245fbb5c4d6d4a0b4896251275727a534c4955cd
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2025-12-12 11:18:54 +0530

    eslint and prettier setupe and code formatting done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send("hello from accquisitons")
-})
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'hello from accquisitons' });
+});
 
-
-export default app
+export default app;
